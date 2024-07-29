0869107d6473eed124be0dcac721b111b911d65a
Joy-Saliu <joysaliu007@gmail.com>
2024-07-30 00:40:34 +0100

    multiplication table file

---
+++ multiplication_table.sh
@@ -0,0 +1,31 @@
+ #!/bin?bash
+
+
+ echo "Enter a number: "
+ read num
+
+
+ echo "Enter the start of the range: "
+ read start
+
+
+ echo "Enter the end of the range: "
+ read end
+
+
+ echo "Multiplication Table for $num from $start to $end:"
+
+
+ for ((i=start; i<=end; i++)); do
+  product=$((num*i))
+  printf "%2d x %2d = %3d\n" $num $i $product
+ done
+
+
+ echo "Do you want to generate another table? (y/n)"
+ read answer
+
+
+ if [ "$answer" = "n" ]; then 
+   break
+  fi
